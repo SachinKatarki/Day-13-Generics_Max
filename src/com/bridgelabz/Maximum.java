948c158db389be8756dd6758463317ecbe2323b3
Textspot <sachinkatarki1995@gmail.com>
2021-11-13 13:34:25 +0530

    Added:Welcome to the Float Maximum Programme

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,16 +1,16 @@
 package com.bridgelabz;
 
 public class Maximum {
-	public static Integer getmaximum(Integer a, Integer b, Integer c) {
-		Integer max = a;
+	
+	public static Float getmaximum(Float a, Float b, Float c) {
+		Float max = a;
 		if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
 			max = b;
 		else if (c.compareTo(max) > 0)
 			max = c;
-		// System.out.println(max);
+		System.out.println(max);
 		return max;
 
 	}
 }
-
 
